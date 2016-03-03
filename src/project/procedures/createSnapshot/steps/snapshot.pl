7a530cecb26472c4b2e5d3a083404a14356a87c4
lrochette <lrochette@electric-cloud.com>
2016-03-03 09:25:36 -0800

    EF-Utilities v0.2.0.9: Add force option

--- src/project/procedures/createSnapshot/steps/snapshot.pl
+++ src/project/procedures/createSnapshot/steps/snapshot.pl
@@ -10,13 +10,15 @@ my $env   = "$[envName]";
 my $force = "$[force]";
 
 # delete a snapshot with the same name if force mode is on
-if (($force eq "true") || ($force eq "1") {
+if (($force eq "true") || ($force eq "1")) {
 	my ($ok) = InvokeCommander("IgnoreError", 'getSnapshot', $proj, $app, $snap);
     if ($ok) {
+    	printf("Deleting snapshot $snap\n");
     	$ec->deleteSnapshot($proj, $app, $snap);
     }
 }
 $ec->createSnapshot($proj, $app, $snap);
+printf("Created snapshot $snap\n");
 
 $[/plugins[EC-Admin]project/scripts/perlLibJSON]
 
