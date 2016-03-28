d4cb7fa997f36c44260b5a917fde18a11ddee9b6
lrochette <lrochette@electric-cloud.com>
2016-03-28 09:19:15 -0700

    EF-Utilities v0.3.0.28:

--- src/project/procedures/createScheduleToTriggerGate/steps/createSchedule.pl
+++ src/project/procedures/createScheduleToTriggerGate/steps/createSchedule.pl
@@ -5,6 +5,7 @@ my ($ok, $json, $errCode, $errMsg) = InvokeCommander("",
     'beginDate' => "$[date]",
     'startTime' => "$[time]",
     'procedureName' => "triggerPipelineGateOnSchedule",
+    'projectName' => "$[/myPipelineRuntime/projectName]",
     'misfirePolicy' => "runOnce",
     actualParameter => [
         {actualParameterName => 'stageName', value => "$[stage]"},
