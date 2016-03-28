1cb7e64814b76788f1af9b26d8af7c2c407fb69e
lrochette <lrochette@electric-cloud.com>
2016-03-28 06:37:56 -0700

    EF-Utilities v0.3.0.21:

--- src/project/procedures/createScheduleToTriggerGate/steps/addPermission.pl
+++ src/project/procedures/createScheduleToTriggerGate/steps/addPermission.pl
@@ -3,6 +3,7 @@ $[/plugins[EC-Admin]project/scripts/perlHeaderJSON]
 my ($ok, $json, $errCode, $errMsg) = InvokeCommander("",
   'createAclEntry', 'user', "project: $[/myProject/projectName]", 
   {
+     projectName => "$[/myPipelineRuntime/projectName]",
      pipelineName => "$[/myPipelineRuntime]", 
      readPrivilege    => "allow",
      executePrivilege => "allow",
