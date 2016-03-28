d284053bebb3187f1582f9376f3f12995a35a1ed
lrochette <lrochette@electric-cloud.com>
2016-03-28 06:09:51 -0700

    EF-Utilities v0.3.0.18:

--- src/out/project/manifest.pl
+++ src/out/project/manifest.pl
@@ -3,6 +3,6 @@
 	['//project/procedure[procedureName="createScheduleToTriggerGate"]/step[stepName="Create Schedule"]/command', 'procedures/createScheduleToTriggerGate/steps/Create Schedule.pl'],
 	['//project/procedure[procedureName="createScheduleToTriggerGate"]/step[stepName="Create Link"]/command', 'procedures/createScheduleToTriggerGate/steps/Create Link.sh'],
 	['//project/procedure[procedureName="createSnapshot"]/step[stepName="snapshot"]/command', 'procedures/createSnapshot/steps/snapshot.pl'],
-	['//project/procedure[procedureName="triggerPipelineGateOnSchedule"]/step[stepName="Trigger"]/command', 'procedures/triggerPipelineGateOnSchedule/steps/Trigger.sh'],
+	['//project/procedure[procedureName="triggerPipelineGateOnSchedule"]/step[stepName="Trigger"]/command', 'procedures/triggerPipelineGateOnSchedule/steps/Trigger.pl'],
 	['//project/procedure[procedureName="triggerPipelineGateOnSchedule"]/step[stepName="Delete Schedule"]/command', 'procedures/triggerPipelineGateOnSchedule/steps/Delete Schedule.sh'],
 );
