85ca474325ce2448680aef3df62f01ddab801716
lrochette <lrochette@electric-cloud.com>
2016-03-29 09:17:48 -0700

    EF-Utilities v0.3.3.41: Fix #4 stage link is hardcoded

--- src/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
+++ src/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
@@ -1,7 +1,7 @@
 $[/plugins[EC-Admin]project/scripts/perlHeaderJSON]
 
 my ($ok, $json, $errCode, $errMSg) = InvokeCommander("",
-  'setProperty', "/myPipelineStageRuntime/ec_summary/QA Schedule",
+  'setProperty', "/myPipelineStageRuntime/ec_summary/$[stage] Schedule",
   {
 		value =>  '<html><pre>$[date] - $[time] UTC
 <a href="/commander/link/editSchedule/projects/BoA Testing/schedules/$[/myPipelineRuntime]">Definition</a></pre></html>'
