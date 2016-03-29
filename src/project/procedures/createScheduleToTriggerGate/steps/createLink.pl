563ba4e90eff1b7336dceb949aafe4b585bb15de
lrochette <lrochette@electric-cloud.com>
2016-03-29 09:40:07 -0700

    EF-Utilities v0.3.3.43:

--- src/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
+++ src/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
@@ -1,10 +1,9 @@
 $[/plugins[EC-Admin]project/scripts/perlHeaderJSON]
 
 my ($ok, $json, $errCode, $errMSg) = InvokeCommander("",
-  'setProperty', "/myPipelineStageRuntime/ec_summary/$[stage] Schedule",
+  'setProperty', "/myPipelineStageRuntime/ec_summary/$[stage] Schedule $[date] $[time]",
   {
-		value =>  '<html><pre>$[date] - $[time] UTC
-<a href="/commander/link/editSchedule/projects/BoA Testing/schedules/$[/myPipelineRuntime]">Definition</a></pre></html>'
+		value =>  '<html><a href="/commander/link/editSchedule/projects/BoA Testing/schedules/$[/myPipelineRuntime]">Definition</a></html>'
    });
 
 $[/plugins[EC-Admin]project/scripts/perlLibJSON]
