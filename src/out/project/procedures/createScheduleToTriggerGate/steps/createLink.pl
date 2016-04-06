425a3672176ac442d80210033f6ab2233a7452ff
lrochette <lrochette@electric-cloud.com>
2016-04-05 21:47:47 -0700

    EF-Utilities v0.3.6.48: Fix #9 ignore duplicateAclEntry

--- src/out/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
+++ src/out/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
@@ -3,7 +3,7 @@ $[/plugins[EC-Admin]project/scripts/perlHeaderJSON]
 my ($ok, $json, $errCode, $errMSg) = InvokeCommander("",
   'setProperty', "/myPipelineStageRuntime/ec_summary/$[stage] Schedule $[date] $[time]",
   {
-		value =>  '<html><a href="/commander/link/editSchedule/projects/BoA Testing/schedules/$[/myPipelineRuntime]">Definition</a></html>'
+		value =>  '<html><a href="/commander/link/editSchedule/projects/BoA Testing/schedules/$[stage]_$[/myPipelineRuntime]">Definition</a></html>'
    });
 
 $[/plugins[EC-Admin]project/scripts/perlLibJSON]
