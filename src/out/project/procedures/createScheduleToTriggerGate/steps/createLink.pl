4f54de39fefca3e19642decb06deb129ed93c8e1
lrochette <lrochette@electric-cloud.com>
2016-04-07 20:03:34 -0700

    EF-Utilities v1.0.0.58: Fix #12 remove EC-Admin dependency

--- src/out/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
+++ src/out/project/procedures/createScheduleToTriggerGate/steps/createLink.pl
@@ -3,7 +3,7 @@ $[/myProject/scripts/perlHeaderJSON]
 my ($ok, $json, $errCode, $errMSg) = InvokeCommander("",
   'setProperty', "/myPipelineStageRuntime/ec_summary/$[stage] Schedule $[date] $[time]",
   {
-		value =>  '<html><a href="/commander/link/editSchedule/projects/$[/myPipelineRuntime/projectName]">Definition</a></html>'
+		value =>  '<html><a href="/commander/link/editSchedule/projects/$[/myPipelineRuntime/projectName]/schedules/$[stage]_$[/myPipelineRuntime]">Definition</a></html>'
    });
 
 $[/myProject/scripts/perlLibJSON]
