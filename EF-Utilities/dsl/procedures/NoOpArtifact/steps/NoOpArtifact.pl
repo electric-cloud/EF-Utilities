d85198d68bf5a5d3c47cdba7da26ab9e4458056b
Polina Shubina <925043@gmail.com>
2022-03-13 15:13:55 +0200

    PDK version of the Utilities plugin

---
+++ EF-Utilities/dsl/procedures/NoOpArtifact/steps/NoOpArtifact.pl
@@ -0,0 +1,6 @@
+$[/myProject/perl/core/scripts/preamble.pl]
+
+use FlowPlugin::EFUtilities;
+# Auto generated code of plugin step
+# Go to dsl/properties/perl/lib/EC/Plugin/EFUtilities.pm and change the function "noOpArtifact"
+FlowPlugin::EFUtilities->runStep('NoOpArtifact', 'NoOpArtifact', 'noOpArtifact');
---
+++ EF-Utilities/dsl/procedures/Seedenvironmentinventory/steps/Seedenvironmentinventory.pl
@@ -0,0 +1,6 @@
+$[/myProject/perl/core/scripts/preamble.pl]
+
+use FlowPlugin::EFUtilities;
+# Auto generated code of plugin step
+# Go to dsl/properties/perl/lib/EC/Plugin/EFUtilities.pm and change the function "seedEnvironmentInventory"
+FlowPlugin::EFUtilities->runStep('Seed environment inventory', 'Seed environment inventory', 'seedEnvironmentInventory');
